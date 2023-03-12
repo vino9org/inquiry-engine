50fdba83c689a34119d6824b37363a51d05ddf66
Li Lin <guru.lin@gmail.com>
2023-03-12 17:24:43 +0800

    spring initializer generated code

---
+++ src/main/java/net/vino9/vinobank/inqeng/InquiryEngineApplication.java
@@ -0,0 +1,13 @@
+package net.vino9.vinobank.inqeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquiryEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InquiryEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/vino9/vinobank/inqeng/InquiryEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package net.vino9.vinobank.inqeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InquiryEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
